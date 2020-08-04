6af9c0cad98a5e4ad97be7455db7d28c4c98ee24
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-08-04 12:40:17 -0700

    API returning data and create user card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+// import { v4 as uuid } from 'uuid';
+// import styled from 'styled-components'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const username = "chaycesol"
+const base_url = `https://api.github.com/users/${username}`
+const follow_url = `https://api.github.com/users/${username}/followers`
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+      followers: []
+    }
+  }
+
+componentDidMount() {
+  axios
+      .get(base_url)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          data: res.data
+        });
+        console.log(res.data)
+      })
+      .then(() =>{
+        axios.get(follow_url).then(res => {
+          this.setState({
+            ...this.state,
+            followers: res.data
+          })
+          console.log(res.data)
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+
+componentDidUpdate(prevProps, prevState) {
+  if(prevState.users !== this.state.users) {
+    axios.get(base_url)
+    .then(res => {
+      this.setState({
+        ...this.state,
+        data: res.data        
+    });
+  })
+  .catch(err => console.log(err));
+}
+}
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Github UserCards: React Version</h1>
+        <div className="userCards">
+          <div>
+            Card Place Holder
+          </div>
+        </div>
+      </div>
+     );
+   }
 }
 
 export default App;
